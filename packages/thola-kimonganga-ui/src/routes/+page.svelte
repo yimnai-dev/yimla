0ffc6f5b4690fe31f3875fbf8f3c053d00003f1c
yimnai-dev <yimnai-dev>
2024-05-16 11:16:49 +0100

    recreated

--- packages/thola-kimonganga-ui/src/routes/+page.svelte
+++ packages/thola-kimonganga-ui/src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
 
-	const app = $derived.by(() => {
+	let app = $derived.by(() => {
 		if (dev) {
 			return 'thola-client';
 		}
-		const [subdomain] = $page.url.host;
+		const [subdomain] = $page.url.host.split('.');
 		return subdomain;
 	});
 </script>
 
-<h1>Welcome to Thola Kimonganga App</h1>
-<p>You are currently looking at the {app} UI</p>
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation for
+	tholaKimonganga: {app}
+</p>
