c6e9ca3e4fea10c8ea7e6fb4a97912c9bd929656
yimnai-dev <yimnai-dev>
2024-05-16 10:50:58 +0100

    cleared up

--- node_modules/.pnpm/nx@18.2.4/node_modules/nx/src/daemon/daemon-project-graph-error.d.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectGraph } from '../config/project-graph';
-import { ConfigurationSourceMaps } from '../project-graph/utils/project-configuration-utils';
-export declare class DaemonProjectGraphError extends Error {
-    errors: any[];
-    readonly projectGraph: ProjectGraph;
-    readonly sourceMaps: ConfigurationSourceMaps;
-    constructor(errors: any[], projectGraph: ProjectGraph, sourceMaps: ConfigurationSourceMaps);
-}
