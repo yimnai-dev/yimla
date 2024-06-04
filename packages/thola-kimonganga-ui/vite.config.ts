5456445a32618cd4cf80ca0811667ab43dec7463
yimnai-dev <yimnai-dev>
2024-06-04 14:42:53 +0100

    fix(tidy):fix

--- packages/thola-kimonganga-ui/vite.config.ts
+++ packages/thola-kimonganga-ui/vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-})
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	test: {
+// 		include: ['src/**/*.{test,spec}.{js,ts}']
+// 	},
+// })
+
+export default defineConfig(({ mode }) => {
+	Object.assign(process.env, loadEnv(mode, process.cwd()));
+	return {
+		plugins: [sveltekit()],
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		},
+	}
+}) 
