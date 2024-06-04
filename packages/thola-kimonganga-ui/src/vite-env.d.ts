156983de329294b6fb282118d08403ec77c16acf
yimnai-dev <yimnai-dev>
2024-06-04 14:31:09 +0100

    fix(tidy): add production env variables to wrangler.toml

--- packages/thola-kimonganga-ui/src/vite-env.d.ts
+++ packages/thola-kimonganga-ui/src/vite-env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
 	readonly VITE_MAPBOX_API_KEY: string;
 	readonly VITE_STRIPE_PUBLISHABLE_TEST_KEY: string;
 	readonly VITE_STRIPE_PRICING_TABLE_TEST_ID: string;
+	readonly MAPBOX_API_KEY: string;
+	readonly STRIPE_PUBLISHABLE_TEST_KEY: string;
+	readonly STRIPE_PRICING_TABLE_TEST_ID: string;
 	// more env variables...
 }
 
--- packages/thola-kimonganga-ui/vite.config.ts
+++ packages/thola-kimonganga-ui/vite.config.ts
@@ -1,14 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { loadEnv } from 'vite';
+// import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
-	process.env = { ...process.env, ...env };
-	return {
-		plugins: [sveltekit()],
+export default defineConfig({
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
-	}
 })
+
+
+// export default defineConfig(({ mode }) => {
+// 	const env = loadEnv(mode, process.cwd());
+// 	process.env = { ...process.env, ...env };
+// 	return {
+// 		plugins: [sveltekit()],
+// 	test: {
+// 		include: ['src/**/*.{test,spec}.{js,ts}']
+// 	},
+// 	}
+// })
