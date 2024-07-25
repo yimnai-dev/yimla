53c084b60068d2851c5fcb7d16fdbaf6c40af1ff
yimnai-dev <yimnai-dev>
2024-07-25 17:39:37 +0100

    troubleshooting complete

--- packages/thola-kimonganga-ui/src/lib/components/shared/medication-table-wrapper.svelte
+++ packages/thola-kimonganga-ui/src/lib/components/shared/medication-table-wrapper.svelte
@@ -15,13 +15,6 @@
 		)
 	);
 
-	medicationListQuery.subscribe($q => {
-		console.table({
-			data: $q.data,
-			error: $q.error
-		})
-	})
-
 	let medications = $derived.by(() => {
 		if (
 			!$medicationListQuery.data ||
